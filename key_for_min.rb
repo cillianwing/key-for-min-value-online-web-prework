df7e98ab931015da24699e97f6a4c4bd1a1aca96
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-06 15:26:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  key_array = []
+  value_array = []
+  name_hash.each do |key, value|
+    if value <= value_array[0] || value_array[0] == nil 
+      value_array = [value] 
+      key_array = [key]
 end
