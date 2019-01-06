66132cb93cfa08184f6ec0c7fab0af969189b719
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-06 15:16:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,9 @@
 
 def key_for_min_value(name_hash)
   min_array = []
+  name_hash.each do |key, value|
+    if value <= min_array[0] || min_array[0] == nil 
+      min_array = key 
+    end
+  end
 end
